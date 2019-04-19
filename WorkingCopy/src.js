393fd0b1dfb641720adb7c696842be211a6bb0f4
uprime22 <pub@uprime.org>
2019-04-19 11:04:44 +0900

    test undo

---
+++ WorkingCopy/src.js
@@ -0,0 +1,38 @@
+var my = {};
+
+// Timestamp;
+function Zero(n){return (n<10) ? '0'+n : n};
+var d = new Date();
+my.year=d.getFullYear();
+my.month=Zero(
+    d.getMonth()+1);
+my.date=Zero(
+    d.getDate());
+
+
+var now = 
+my.year
++'-'+ my.month
++'-'+ my.date
++' '+Zero(d.getHours())+':'+Zero(d.getMinutes())+'\n';
+
+my.text = now + T.whole + '\n' + '---'+ '\n';
+// your key
+my.key = 'hogehoge';
+// target repogitory
+my.repo = 'my%20repo';
+// path name
+my.path = my.year+'_'+my.month+'_'+'log.txt';
+
+T( 'urlScheme', {
+  url: 'working-copy://x-callback-url/write?key='
+  +my.key
+  +'&repo='
+  +my.repo
+  +'&path='
+  +my.path
++'&mode=prepend&clipboard=no&text='
++ encodeURIComponent(my.text),
+  option: 'none'
+} );
+
